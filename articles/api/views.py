13e0e38b9d55ef3ede6a43075eb96c0c8066db11
Brian Presser <bmpresser@gmail.com>
2020-03-28 23:27:33 -0700

    Adding CRUD functions

--- articles/api/views.py
+++ articles/api/views.py
@@ -1,13 +1,37 @@
 from rest_framework.generics import ListAPIView
-from rest_framework.generics import RetrieveAPIView
+# from rest_framework.generics import (
+#     RetrieveAPIView, 
+#     RetrieveAPIView, 
+#     CreateAPIView,
+#     UpdateAPIView,
+#     DestroyAPIView
+# )
 
 from articles.models import Article
 from .serializers import ArticleSerializer
 
-class ArticleListView(ListAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
+# class ArticleListView(ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
 
-class ArticleDetailView(RetrieveAPIView):
-    queryset = Article.objects.all()
+# class ArticleDetailView(RetrieveAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+# class ArticleCreateView(CreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+# class ArticleUpdateView(UpdateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+# class ArticleDeleteView(DestroyAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+from rest_framework import viewsets
+
+class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
